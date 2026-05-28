725e8af6cb4d412dae3d1b845435b8fb897e3733
Druce <lapuzin@tochka.com>
2026-05-29 02:04:12 +0500

    Начал накидывать приложение

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -1,7 +1,12 @@
 export const SETTINGS = {
+  get PORT() {
+    return process.env.PORT ?? 5001;
+  },
+
   get MONGO_DB_URL() {
     return process.env.MONGO_DB_URL ?? '';
   },
+
   get MONGO_DB_NAME() {
     return process.env.MONGO_DB_NAME;
   },
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { SETTINGS } from './core/settings/settings';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
-  await app.listen(5_001);
+  await app.listen(SETTINGS.PORT);
 }
 
 bootstrap();
